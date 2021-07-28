c0a24dbf4097c1d541e5377604ccaa3517cfd005
shivam sharma <shivam.cloud02@gmail.com>
2021-07-28 23:58:22 +0530

    Literals

---
+++ Literals.java
@@ -0,0 +1,42 @@
+/*
+class PrintLiterals{
+	void show(){
+		int i1 = 32324; //Decimal Literal
+		int i2 = 01234567; //Octal Literal
+		int i3 = 18; //Not Octal Literal
+		int i4 = 1010101; // Decimal Literal
+		int i5 = 0x123ABC; // Hexadecimal Literal
+		int i6 = 0xABCDEF; //Hexadecimal Literal
+		
+		System.out.println(i1);
+		System.out.println(i2);
+		System.out.println(i3);
+		System.out.println(i4);
+		System.out.println(i5);
+		System.out.println(i6);
+		//System.out.println(i7);
+		
+	}
+}
+*/
+class Literals{
+	public static void main(String[] args){
+		/*PrintLiterals obj = new PrintLiterals();
+		obj.show();	
+		*/
+		int i1 = 32324; //Decimal Literal
+		int i2 = 01234567; //Octal Literal
+		int i3 = 18; //Not Octal Literal
+		int i4 = 1010101; // Decimal Literal
+		int i5 = 0x123ABC; // Hexadecimal Literal
+		int i6 = 0xABCDEF; //Hexadecimal Literal
+		
+		System.out.println(i1);
+		System.out.println(i2);
+		System.out.println(i3);
+		System.out.println(i4);
+		System.out.println(i5);
+		System.out.println(i6);
+		//System.out.println(i7);
+	}
+}
